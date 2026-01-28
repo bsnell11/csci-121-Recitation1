6ac1e7b4ebb2b3a47edf1aa53738c2ac2c1fa02e
briannasnell <brisnell11@gmail.com>
2026-01-27 23:05:26 -0500

    Added all 3 outputs. (Normal, Reverse, cast).

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -11,6 +11,20 @@ public class BasicInput {
 
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
+        System.out.println("Enter double:");
+        userDouble = scnr.nextDouble();
+        System.out.println("Enter character:");
+        userChar = scnr.next().charAt(0);
+
+        System.out.println("Enter string:");
+        userString = scnr.next();
+
+        System.out.println(userInt + " " + userDouble + " " + userChar + " " + userString);
+        System.out.println(userString + " " + userChar + " " + userDouble + " " + userInt);
+
+        int castInt = (int)userDouble;
+
+        System.out.println(userDouble + " cast to an integer is " + castInt);
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
